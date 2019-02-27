941eb894bea7b53d03fa9fe4b431fff0117c2400
AirDur <ralphagris@gmail.com>
2019-02-27 13:32:03 +0100

    erreur lors d'ajout de tâche / tâche fini / suppression des tâches finie

--- Angular/js/todoAjoutTaskApp.js
+++ Angular/js/todoAjoutTaskApp.js
@@ -1,14 +1,58 @@
-var todoAjoutTaskApp = angular.module('todoAjoutTaskApp', []);
-todoAjoutTaskApp.controller('TestCtrl1', ['$scope', function($scope) {
+var todoApp = angular.module('todoApp', []);
+todoApp.controller('ctrlTodo', ['$scope', function($scope) {
 
+    //initialisation
     $scope.init = function() {
-        $scope.TaskSet = [];
+        $scope.tabOfTasks = [];
+        $scope.checked = [];
     }
 
+    $scope.styleChecked = {
+        "text-decoration" : "line-through"
+    }
+
+    //Ajout d'une task. Affiche une erreur si déjà en double.
     $scope.addTask = function() {
-        $scope.TaskSet.push($scope.taskName);
-        $scope.taskName = "";
+        if($scope.tabOfTasks.indexOf($scope.taskName) == -1) {
+            $scope.tabOfTasks.push($scope.taskName);  
+            var index = $scope.tabOfTasks.indexOf($scope.taskName); 
+            $scope.checked[index] = false;
+            $scope.taskName = "";
+            $scope.addTaskError = "";
+        } else {
+            $scope.addTaskError = "Erreur : tâche déjà existante.";
+        }
     };
+
+    //Supprime une tâche en particulier  
+    $scope.delTask = function(index) {
+        $scope.tabOfTasks.splice(index,1);
+        $scope.checked.splice(index,1);
+    }
+
+    //Supprime les tâches "finies"
+    $scope.delCheckedTask = function() {
+        for(var i = 0; i <= $scope.checked.length; i++) {
+            if($scope.checked[i]) {
+                $scope.delTask(i);
+                i = $scope.checked.length+1;
+                $scope.delCheckedTask();
+            }
+        }
+    }
+
+    //barre une tache faite.
+    $scope.taskDone = function(aTask) {
+        var index = $scope.tabOfTasks.indexOf(aTask);
+        if($scope.checked[index]) {
+            return $scope.styleChecked;
+        }         
+        else {
+            return "";
+        }
+    }
 
     $scope.init();
 }]);
+
+  
